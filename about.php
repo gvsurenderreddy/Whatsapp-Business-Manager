7df8d30b44028442718c878b8894088e8e60bf04
Gundo Sifhufhi <sifhufhisg@gmail.com>
2022-12-31 03:19:38 +0200

    About Boilerplate

--- about.php
+++ about.php
@@ -2,9 +2,62 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>About WhatsappMan</title>
+<?php include 'includes/head.php';?>
+
 </head>
 <body>
+<?php include 'navbar.php';?>
+
+<!-- About Whatsappman , 3 columns with parallax -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">About WhatsappMan</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="text-center">WhatsappMan is a free service that allows you to send messages to your friends and family on Whatsapp.</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Send messages</h5>
+                    <p class="card-text text-center">Send messages to your friends and family on Whatsapp.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Send messages</h5>
+                    <p class="card-text text-center">Send messages to your friends and family on Whatsapp.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Send messages</h5>
+                    <p class="card-text text-center">Send messages to your friends and family on Whatsapp.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- About Whatsappman , 3 columns with parallax end -->
+
+<!-- Footer -->
+
+<?php include 'footer.php';?>
+
 
 </body>
 </html>
