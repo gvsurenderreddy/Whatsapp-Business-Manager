e8f33166003ec9d3e5b3e65f8f65916a813430f6
Gundo Sifhufhi <sifhufhisg@gmail.com>
2022-12-31 03:20:30 +0200

    Contact form boilerplate

--- contact.php
+++ contact.php
@@ -21,6 +21,85 @@
 
 </head>
 <body>
+<!-- navbar -->
+
+<?php include 'navbar.php'; ?>
+<!-- Send page with 3 columns, left menu , middle main content form and other things, right menu essential links -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Menu</h5>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group list-group-item"><a href="index.php">Home</a></li>
+                        <li class="list-group list-group-item"><a href="settings.php">Settings</a></li>
+                        <li class="list-group list-group-item"><a href="send.php">Send</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Contact Us</h5>
+                </div>
+                <!-- Constraints:
+A text message can be a max of 4096 characters long.
+
+-->
+
+                <div class="card-body">
+                    <form action="contact.php" method="post">
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="message" class="col-sm-2 col-form-label">Message</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Essential Links</h5>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group list-group-item"><a href="https://www.facebook.com/">Facebook</a></li>
+                        <li class="list-group list-group-item"><a href="https://www.instagram.com/">Instagram</a></li>
+                        <li class="list-group list-group-item"><a href="https://www.twitter.com/">Twitter</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer -->
+<?php include 'footer.php'; ?>
 
 </body>
 </html>
