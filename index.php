cd39ef61dc1cbb0bb51e553839002b670d8574a2
Gundo Sifhufhi <sifhufhisg@gmail.com>
2022-12-31 03:21:08 +0200

    Abstract Navbar

--- index.php
+++ index.php
@@ -50,36 +50,8 @@
 </head>
 <body>
 <!-- Build slick ui with  product showcase, sign up form that is powered by javascript, footer with sliding up animation -->
-<!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">WhatsappMan</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="signup.php">Sign Up</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="about.php">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact</a>
-            </li>
 
-        </ul>
-    </div>
-</nav>
-<!-- end of navbar -->
-<br>
-<br>
+<?php include 'navbar.php'; ?>
 <br>
 <!-- pricings cards horizontal -->
 <div class="container">
